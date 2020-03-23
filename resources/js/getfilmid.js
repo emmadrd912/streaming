1c4368f5304340b34f0afe9d353bf318effdec4c
Lucasmouchague <lucas.mouchague@ynov.com>
2020-03-23 13:57:02 +0000

    example how to extract metadata

--- resources/js/getfilmid.js
+++ resources/js/getfilmid.js
@@ -1,12 +1,18 @@
-import TMDB from "./Classes/TMDB.js";
-function successCB(data) {
-    // console.log(data);
+const theMovieDb = require('./TMDB');
+
+function searchfilmbyname(name)
+{
+  const film = theMovieDb.search.getMovie({"query":name}, getfilmidbyname, errorgetfilmidbyname);
+  
+  function getfilmidbyname(data) 
+  {
     let stored = JSON.parse(data);
     console.log(stored.results[0].id);
   };
           
-  function errorCB(data) {
-            console.log("Error callback: " + data);
-      };
-  
-  const film = TMDB.theMovieDb.search.getMovie({"query":"Fight Club"}, successCB, errorCB);
+  function errorgetfilmidbyname(data) 
+    {
+      console.log("Error callback: " + data);
+    };
+};
+searchfilmbyname('Ad Astra')
