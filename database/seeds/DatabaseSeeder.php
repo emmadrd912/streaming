ab6f1a16f074162a15682063391f71108bf58a55
Lucasmouchague <lucas.mouchague@ynov.com>
2020-03-30 17:23:16 +0100

    tamere

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(CountriesTableSeeder::class);
+        $this->call(CountriesTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(PlanTableSeeder::class);
