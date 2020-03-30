2bb4fbbf8765eedc12a1627648b37708f8624631
Lucasmouchague <lucas.mouchague@ynov.com>
2020-03-30 16:25:27 +0100

    fix radio button

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,6 +2,7 @@ import Helpers from "./Helpers.js";
 import Serie from "./Classes/Serie.js";
 import Film from "./Classes/Film.js";
 
+
 const apiKey = "97719463bea4bd4b5902c1a735c0556a";
 
 const traiterMedia = (data, type) => {
--- resources/views/contents/create.blade.php
+++ resources/views/contents/create.blade.php
@@ -4,14 +4,15 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+        <div class="card">
                 <div class="card-header">Ajouter un contenu</div>
                 <div class="card-body">
                 <form>
-                <label class="radio-inline"><input type="radio" name="ContentSelect" checked>Film</label>
-                <label class="radio-inline"><input type="radio" name="ContentSelect">Série</label>
+                <label class="radio-inline"><input type="radio" onclick="magueule('film');" name="select" id=filmcheck checked>Film</label>
+                <label class="radio-inline"><input type="radio" onclick="magueule('serie');" name="select" id=seriecheck >Série</label>
                 </form>
-                <form action="{{ route('contents.moviestore')}}" method="POST" enctype="multipart/form-data" id="video">
+                <div id="videodiv">
+                    <form action="{{ route('contents.moviestore')}}" method="POST" enctype="multipart/form-data" id="video">
                         @csrf
                         <div class="field">
                             <div class="form-group">
@@ -29,7 +30,32 @@
                         </div>
                         <div class="field">
                             <div class="control">
-                                <button class="btn btn-primary" type="submit">Add video</button>
+                                <button class="btn btn-primary" type="submit">Add movie</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+                <div id=seriediv>
+                    <form action="{{ route('contents.moviestore')}}" method="POST" enctype="multipart/form-data" id="serie">
+                        @csrf
+                        <div class="field">
+                            <div class="form-group">
+                                <label for="serie_name">Serie name :</label>
+                                <input type='text' class="form-control" name="seriename" placeholder="Name of the serie"></input>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <div class="control">
+                                <input class="input" type="file" name="serie">
+                            </div>
+                            @if($errors->has('serie'))
+                                <p class="help is-danger">{{ $errors->first('serie') }}</p>
+                            @endif
+                        </div>
+                        <div class="field">
+                            <div class="control">
+                                <button class="btn btn-primary" type="submit">Add serie</button>
                             </div>
                         </div>
                     </form>
@@ -38,4 +64,21 @@
         </div>
     </div>
 </div>
+<script>
+document.getElementById('videodiv').style.display='block';
+document.getElementById('seriediv').style.display='none';
+function magueule(value)
+{
+    if(value=='film')
+    { 
+        document.getElementById('videodiv').style.display='block';
+        document.getElementById('seriediv').style.display='none';  
+    }
+    else if(value=='serie') 
+    { 
+        document.getElementById('seriediv').style.display='block';
+        document.getElementById('videodiv').style.display='none'; 
+    }
+}
+</script>
 @endsection
