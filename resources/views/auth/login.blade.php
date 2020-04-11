1b457352be7388a445e792a49e73f950c0570a42
emmadrd912 <emma.durand@ynov.com>
2020-04-12 00:00:28 +0200

    oublie color

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.log')
 
 @section('content')
-<div class="container" style="margin-top:10%;">
+<div class="container blacktext" style="margin-top:10%;">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
